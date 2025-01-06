e8d634c66db8c962fc7591e64242e105da4669c8
ujala <singhujala2004@gmail.com>
2025-01-06 13:25:17 +0530

    Remove unused files and dependencies from node_modules

--- routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loginUser  } from '../controllers/auth.controller.js';
-
-const router = express.Router();
-
-router.post('/',loginUser); 
-
-
-export default router;
